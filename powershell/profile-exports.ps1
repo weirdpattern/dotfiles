b0b14ae89803205eaf5a3575982a576376a85c7d
Patricio Trevino <patricio@weirdpattern.com>
2019-02-05 17:00:27 -0600

    Update profile-exports.ps1

--- powershell/profile-exports.ps1
+++ powershell/profile-exports.ps1
@@ -5,18 +5,24 @@
 # Notepad++ as default editor
 if (Test-Path "${env:programfiles}\Vim\vim80\vim.exe") {
   [Environment]::SetEnvironmentVariable("EDITOR", "${env:programfiles}\Vim\vim80\vim.exe")
+  [Environment]::SetEnvironmentVariable("GIT_EDITOR", "${env:programfiles}\Vim\vim80\vim.exe")
 } elseif (Test-Path "${env:programfiles(x86)}\Vim\vim80\vim.exe") {
   [Environment]::SetEnvironmentVariable("EDITOR", "${env:programfiles(x86)}\Vim\vim80\vim.exe")
+  [Environment]::SetEnvironmentVariable("GIT_EDITOR", "${env:programfiles(x86)}\Vim\vim80\vim.exe")
 } elseif (Test-Path "${env:programfiles}\Notepad++\notepad++.exe") {
   [Environment]::SetEnvironmentVariable("EDITOR", "$env:programfiles\Notepad++\notepad++.exe")
+  [Environment]::SetEnvironmentVariable("GIT_EDITOR", "$env:programfiles\Notepad++\notepad++.exe")
 } elseif (Test-Path "${env:programfiles(x86)}\Notepad++\notepad++.exe") {
   [Environment]::SetEnvironmentVariable("EDITOR", "${env:programfiles(x86)}\Notepad++\notepad++.exe")
+  [Environment]::SetEnvironmentVariable("GIT_EDITOR", "${env:programfiles(x86)}\Notepad++\notepad++.exe")
 } else {
   [Environment]::SetEnvironmentVariable("EDITOR", "${env:windir}\System32\notepad.exe")
+  [Environment]::SetEnvironmentVariable("GIT_EDITOR", "${env:windir}\System32\notepad.exe")
 }
 
 # user repositories
-[Environment]::SetEnvironmentVariable("Sandbox", "C:\Sandbox");
+[Environment]::SetEnvironmentVariable("Sandbox", "C:\__");
+[Environment]::SetEnvironmentVariable("Deployment", "C:\___");
 
 # user information
 [Environment]::SetEnvironmentVariable("GitUserName", "weirdpattern");
