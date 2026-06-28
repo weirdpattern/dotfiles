cbadcb024b2b46b6f895daf9918ae43f890cb567
Patricio Trevino <patricio.trevino@weirdpattern.com>
2026-06-27 20:05:28 -0500

    Changes to support devcontainers

---
+++ install.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Check if zsh is installed and do nothing if not
+if ! command -v zsh >/dev/null 2>&1; then
+  echo "zsh is not installed. Please install zsh and rerun this script."
+  exit 1
+fi
+
+# Get the path of zsh
+ZSH_PATH=$(which zsh)
+
+# Get the directory of the ZSH plugins
+PLUGINS_DIR="$HOME/.zsh_addons"
+
+# Get the directory of the dotfiles
+DOTFILES_DIR=$(dirname "$(realpath "$0")")
+
+# Remove existing files if they exist
+rm -f "$HOME/.aliases" > /dev/null 2>&1
+rm -f "$HOME/.zshrc" > /dev/null 2>&1
+rm -f "$HOME/.config/" > /dev/null 2>&1
+rm -rf "$PLUGINS_DIR" > /dev/null 2>&1
+rm -rf "$HOME/.gitconfig" > /dev/null 2>&1
+rm -rf "$HOME/.config/zsh" > /dev/null 2>&1
+
+# Creates root directories if they don't exist
+mkdir -p "$HOME/.config"
+mkdir -p "$PLUGINS_DIR"
+mkdir -p "$HOME/.config/zsh"
+mkdir -p "$HOME/.config/zsh/aliases"
+mkdir -p "$HOME/.config/zsh/exports"
+mkdir -p "$HOME/.config/zsh/registrations"
+
+echo "ZDOTDIR=$HOME/.config/zsh" > "$HOME/.zshenv"
+
+# Copy bat files to .config
+ln -s "$DOTFILES_DIR/configs/bat/.config" "$HOME/.config/bat"
+ln -s "$DOTFILES_DIR/configs/bat/zsh/aliases/bat.ps1" "$HOME/.config/zsh/aliases/bat.ps1"
+ln -s "$DOTFILES_DIR/configs/bat/zsh/exports/bat.ps1" "$HOME/.config/zsh/exports/bat.ps1"
+
+# Copy delta files to .config
+ln -s "$DOTFILES_DIR/configs/delta/.config" "$HOME/.config/delta"
+git config --global include.path "$HOME/.config/delta/themes"
+git config --global core.pager "delta --paging=always"
+git config --global interactive.diffFilter "delta --color-only"
+git config --global merge.conflictStyle "zdiff3"
+git config --global delta.navigate true
+git config --global delta.side-by-side true
+git config --global delta.hyperlinks true
+git config --global delta.features "gruvbox"
+
+# Copy eza files to .config
+ln -s "$DOTFILES_DIR/configs/eza/.config" "$HOME/.config/eza"
+ln -s "$DOTFILES_DIR/configs/eza/zsh/aliases/eza.ps1" "$HOME/.config/zsh/aliases/eza.ps1"
+ln -s "$DOTFILES_DIR/configs/eza/zsh/exports/eza.ps1" "$HOME/.config/zsh/exports/eza.ps1"
+
+# Copy fselect files to .config
+ln -s "$DOTFILES_DIR/configs/fzf/zsh/aliases/fselect.sh" "$(CONFIGDIR)/zsh/aliases/fselect.sh"
+
+# Copy fzf files to .config
+ln -s "$DOTFILES_DIR/configs/fzf/zsh/aliases/fzf.sh" "$HOME/.config/zsh/aliases/fzf.sh"
+ln -s "$DOTFILES_DIR/configs/fzf/zsh/exports/fzf.sh" "$HOME/.config/zsh/exports/fzf.sh"
+ln -s "$DOTFILES_DIR/configs/fzf/zsh/registrations/fzf.sh" "$HOME/.config/zsh/registrations/fzf.sh"
+
+# Copy git files to .config
+ln -s "$DOTFILES_DIR/configs/git/.config/config" "$HOME/.config/git/config"
+
+# Copy jq files to .config
+ln -s "$DOTFILES_DIR/configs/jq/zsh/aliases/jq.sh" "$HOME/.config/zsh/aliases/jq.sh"
+
+# Copy starship files to .config
+ln -s "$DOTFILES_DIR/configs/starship/.config" "$HOME/.config/starship"
+ln -s "$DOTFILES_DIR/configs/starship/zsh/registrations/starship.sh" "$HOME/.config/zsh/registrations/starship.sh"
+
+# Copy tldr files to .config
+ln -s "$DOTFILES_DIR/configs/tldr/zsh/aliases/tldr.sh" "$HOME/.config/zsh/aliases/tldr.sh"
+
+# Copy zsh files to .config
+ln -s "$DOTFILES_DIR/configs/zsh/.zshrc" "$HOME/.config/zsh/.zshrc"
+
+# Configure vscode
+git config --global delta.hyperlinks-file-link-format "vscode://file/{path}:{line}"
